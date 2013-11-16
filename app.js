b5334912b5ba8762f90b4d62f97988b7b3e75ba7
Derick Leony <dleony@gmail.com>
2013-11-16 14:05:42 +0100

    Creating templates and index page

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(stylus.middleware(
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.end('Shop guide message!');
+    res.render('index', {
+        title: 'Home'
+    });
 });
 app.listen(config.server.port)
